d2f232ef40138922752d9b891cce8f1952e819d1
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-26 20:23:04 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,17 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+def introduction
+
+end
+
+def introduction_with_language
+
+end
+
+def introduction_with_language_optional(name, language = "computer")
+  
+end
 
 
 
